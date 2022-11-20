889dce234aaf35bc7b4399d648aff2a497c2d7e7
Michael Leow <leow@sinarproject.org>
2022-11-20 15:53:11 +0800

    Summary: Implemented matching of name with Scraped data; catching exceptions

--- cmd/pru15ager/pager.go
+++ cmd/pru15ager/pager.go
@@ -18,15 +18,134 @@ type candidate struct {
 	age  string // COuld be date pattern or word with tahun ..
 }
 
+type matchCandidate struct {
+	code            string
+	name            string
+	candidateDir    string
+	candidateNames  []string
+	matchedFileName string
+}
+
 func Run() {
 	fmt.Println("Runing ..")
 	//getCandidates()
-	downloadCandidates()
+	//downloadCandidates()
+	extractCandidates()
+}
+
+func extractCandidates() {
+	duns := []string{"perlis"}
+	//duns := []string{"perak", "pahang"}
+	for _, dunName := range duns {
+		// Open data from Spreadsheet .. use this instead? so can be written back correctly
+		// and can cut + paste?
+		// Open the data from scaper
+		r, err := script.File("testdata/pru15-" + dunName + "-final.csv").Slice()
+		if err != nil {
+			panic(err)
+		}
+		for _, line := range r {
+			cols := strings.Split(line, ",")
+			// Schema for Final Candidate Data
+			// DUN_ID,NAME,GENDER,AGE
+			if len(cols) != 4 {
+				// DEBUG
+				spew.Dump(line)
+				spew.Dump(cols)
+				panic(fmt.Errorf("WRONG! Must have 4 cols! Got %d", len(cols)))
+			}
+			code := fmt.Sprintf("%05s", cols[0])
+			officialName := cols[1]
+			dataPattern := fmt.Sprintf("testdata/%s", code)
+
+			// For matchCandidateName
+			mc := matchCandidate{
+				code:         code,
+				name:         officialName,
+				candidateDir: dataPattern,
+			}
+			matchCandidateName(&mc)
+			// LAST STEP; get data from here .. if found; otherwise need manual
+			if mc.matchedFileName != "" {
+				fmt.Println("FIND AGE in", mc.matchedFileName)
+			}
+			fmt.Println("================================================================")
+			// DEBUG
+			//spew.Dump(mc)
+			//// For extractCandidateAge
+			//c := candidate{
+			//	name: officialName,
+			//	code: code,
+			//}
+			//extractCandidatesAge(&c)
+			//// After manipulations ..
+			//spew.Dump(c)
+		}
+		// fill it in the file? where it is ordered? match it?
+		// load out; and try to match loosely ..
+	}
+
+}
+
+func matchCandidateName(mc *matchCandidate) {
+	// Look for all files in dataPath
+	candidateFiles, err := script.ListFiles(mc.candidateDir).Slice()
+	if err != nil {
+		panic(err)
+	}
+	//spew.Dump(candidateFiles)
+	candidateFilePath := ""
+	safeName := strings.ReplaceAll(mc.name, " ", "-")
+	fmt.Println("<<<<<<<<<<<<", mc.name, "in", mc.candidateDir, ">>>>>>>>>>")
+	for _, dataFilePath := range candidateFiles {
+		// DEBUG
+		//fmt.Println("Look for:", safeName, "  in", dataFilePath)
+		if strings.Contains(dataFilePath, safeName) {
+			candidateFilePath = dataFilePath
+			break
+		}
+
+		for _, namePart := range strings.Split(safeName, "-") {
+			//spew.Dump(namePart)
+			if strings.Contains(dataFilePath, namePart) {
+				candidateFilePath = dataFilePath
+				break
+			}
+		}
+	}
+	// check if candidates components are there
+	// favor exact match; otherwise pick the most numbers ..
+	if candidateFilePath == "" {
+		// Should NOT get here .. means need to DEBUG!
+		fmt.Println("DEBUG: Look for ", mc.name, "in:")
+		for _, dataFilePath := range candidateFiles {
+			fmt.Println(dataFilePath)
+		}
+		return
+	}
+	mc.matchedFileName = candidateFilePath
+	fmt.Println("FOUND_CHOSEN_PATH:", candidateFilePath)
+
+	return
+}
+
+func extractCandidatesAge(c *candidate) {
+	//ageofCandidate := 2022
+	fmt.Println("Get Profile for", c.name)
+	fmt.Println(fmt.Sprintf("CODE: %s", c.code))
+	safeName := strings.ReplaceAll(c.name, " ", "-")
+	dataPath := fmt.Sprintf("testdata/%s", c.code)
+	fmt.Println("Open:", safeName, ".html in", dataPath)
+
+	// Dump it out as CSV ..
+	//strconv.ParseInt(ageofCandidate, 10, 8)
+	//age := string(ageofCandidate)
+	c.age = "2022"
 }
 
 func downloadCandidates() {
 
-	//duns := []string{"perlis", "perak"}
+	//duns := []string{"perlis"}
 	duns := []string{"perak", "pahang"}
 	for _, dunName := range duns {
 		r, err := script.File("testdata/pru15-" + dunName + ".csv").Slice()
