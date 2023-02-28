2daccdbacac61ed8aed75fdc2fcd6574c4eca951
Michael Leow <leow@sinarproject.org>
2023-03-01 03:02:45 +0800

    Summary: First cut testing google search lib

--- cmd/pru14ager/fuzzy.go
+++ cmd/pru14ager/fuzzy.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/bitfield/script"
-	"github.com/davecgh/go-spew/spew"
+	googlesearch "github.com/rocketlaunchr/google-search"
 )
 
 // FuzzyDownloadCandidatePerPAR go from raw PAR to PAR_ID
@@ -19,13 +18,34 @@ func FuzzyDownloadCandidatePerPAR(state string, pars []string) {
 		fmt.Println("PAR:", parID)
 		candidatesInPAR := candidatesPAR[parID]
 		// Fuzzy download below ...
-		spew.Dump(candidatesInPAR)
+		//spew.Dump(candidatesInPAR)
+		for _, candidate := range candidatesInPAR {
+			fuzzySearch(parID, candidate.name)
+		}
 	}
 }
 
 func fuzzySearch(parID, fullName string) {
 	// For each PAR; use the components to fuzzy match possibility via Google ..
-	possibleCandidatesQuery := ""
-	script.Get(possibleCandidatesQuery)
+
+	fmt.Println(googlesearch.Search(nil, "cars for sale in Toronto, Canada"))
+
+	//baseURL := "https://www.google.com.sg/search"
+	//q := url.QueryEscape(fullName + " \"pru14\"" + " site:pru.sinarharian.com.my")
+	//possibleCandidatesQuery := fmt.Sprintf("%s?q=%s", baseURL, q)
+	//fmt.Println("GET: ", possibleCandidatesQuery)
+	//_, err := script.Get(possibleCandidatesQuery).Stdout()
+	//if err != nil {
+	//	panic(err)
+	//}
+	//script.Get(possibleCandidatesQuery).FilterLine(func(s string) string {
+	//	// Data
+	//	fmt.Println(s)
+	//	// Look for A tag .. with calon in it ..
+	//	if strings.Contains(s, "calon") {
+	//		fmt.Println("LOOK HERE: ", s)
+	//	}
+	//	return ""
+	//})
 
 }
