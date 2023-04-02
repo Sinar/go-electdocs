d9206bf3d3bebdf0a9ad292428f70d0f9490fd76
Michael Leow <leow@sinarproject.org>
2023-04-02 20:49:51 +0800

    Summary: Port from pru14verify; tweak for new data col

---
+++ cmd/pru15verify/main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"fmt"
+	"github.com/davecgh/go-spew/spew"
+	"sync"
+)
+
+var (
+	lookupPAR map[string]par
+	lookupDUN map[string]dun
+)
+
+func init() {
+	// Setup global lookup .. the only allowed use of global :(
+	lookupPAR = LookupPAR()
+	lookupDUN = LookupDUN()
+}
+
+func main() {
+
+	fmt.Println("Welcome to PRU14 Verifier!")
+	// DEBUG
+	ProcessKedah()
+	//ProcessMelaka()
+	//ProcessN9()
+	//ProcessPahang()
+}
+
+func ProcessPahang() {
+	//pars := []string{"P078", "P079"}
+	pars := []string{
+		"P078", "P079", "P080", "P081",
+		"P082", "P083", "P084", "P085",
+		"P086", "P087", "P088", "P089",
+		"P090", "P091",
+	}
+	AssembleResultsPerPAR("PAHANG", pars)
+}
+
+func ProcessN9() {
+	pars := []string{
+		"P126", "P127", "P128", "P129",
+		"P130", "P131", "P132", "P133",
+	}
+	AssembleResultsPerPAR("N9", pars)
+}
+
+func ProcessMelaka() {
+	pars := []string{
+		"P134", "P135", "P136", "P137", "P138", "P139",
+	}
+	AssembleResultsPerPAR("MELAKA", pars)
+}
+
+func ProcessKedah() {
+	// DEBUG
+	//LookupState()
+	//LookupParty()
+	//LookupPAR()
+	// DEBUG
+	//pars := []string{"P006"}
+	pars := []string{"P006", "P007", "P008"}
+	//pars := []string{
+	//	"P004", "P005", "P006", "P007", "P008", "P009",
+	//	"P010", "P011", "P012", "P013", "P014", "P015",
+	//	"P016", "P017", "P018",
+	//}
+
+	//AssembleResultsPerPAR("KEDAH", pars)
+	SanityAssembleResultsPerPAR("KEDAH", pars)
+}
+
+// SanityAssembleResultsPerPAR is stripped down version to ensure col correct ..
+func SanityAssembleResultsPerPAR(state string, pars []string) {
+	// Extract out candidates; much more diverse in PRU15!!
+	candidates := LookupResults(state)
+	// DEBUG
+	spew.Dump(candidates)
+	for _, par := range pars {
+		//SanityTestPARSaluran(par)
+		currentPAR := par
+		//fmt.Println("Processing PAR: ", currentPAR)
+		salurans := LoadPARSaluran(currentPAR)
+		// DEBUG
+		//spew.Dump(salurans)
+		fmt.Println("NO ROWS: ", len(salurans))
+	}
+}
+
+func AssembleResultsPerPAR(state string, pars []string) {
+	candidates := LookupResults(state)
+	// DEBUG
+	//spew.Dump(candidates)
+
+	// For each PAR ID; extract out the first section
+	// lookup from Results; which is ordered by SaluranID: 0,1,2
+	// attach to the candidateVotes
+	// For each pars; extract the number; remove the P
+	// Look up the candidates; ordered by the BallotID
+	var wg sync.WaitGroup
+	prefixData := make([][]string, 0)
+	candidateData := make([][]string, 0)
+	suffixData := make([][]string, 0)
+	for _, par := range pars {
+		//SanityTestPARSaluran(par)
+		currentPAR := par
+		//fmt.Println("Processing PAR: ", currentPAR)
+		salurans := LoadPARSaluran(currentPAR)
+		wg.Add(3) // MUST match the number of concurrent funcs ..
+		go func() {
+			prefixData = append(prefixData,
+				processPrefix(currentPAR, salurans)...,
+			)
+			wg.Done()
+		}()
+		go func() {
+			candidateData = append(candidateData,
+				processCandidates(currentPAR, salurans, candidates)...,
+			)
+			wg.Done()
+		}()
+		go func() {
+			suffixData = append(suffixData,
+				processSuffix(currentPAR, salurans, candidates)...,
+			)
+			wg.Done()
+		}()
+		// Block till done ..
+		wg.Wait()
+		//break
+	}
+	// Assemble the sections ..
+	outputCSV(fmt.Sprintf("testdata/%s-prefix.csv", state), prefixData)
+	outputCSV(fmt.Sprintf("testdata/%s-candidates.csv", state), candidateData)
+	outputCSV(fmt.Sprintf("testdata/%s-suffix.csv", state), suffixData)
+
+}
---
+++ cmd/pru15verify/saluran.go
@@ -0,0 +1,218 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+// 0 - Bil,
+// 1 - No. Kod Daerah Mengundi,
+// 2 - Nama Pusat Mengundi,
+// 3 - Nombor Tempat Mengundi (saluran),
+// 4 - Jumlah kertas undi yang patut berada di dalam peti undi (A),
+// 5 - Bilangan undian oleh pemilih bagi setiap orang calon yang bertanding (B) BALLOT_ID 1,
+// 6 - Bilangan undian oleh pemilih bagi setiap orang calon yang bertanding (B) BALLOT_ID 2,
+// 7 - Bilangan undian oleh pemilih bagi setiap orang calon yang bertanding (B) BALLOT_ID 3,
+// 8 - Bilangan undian oleh pemilih bagi setiap orang calon yang bertanding (B) Jumlah undian oleh pemilih,
+// 9 - Bilangan kertas undi yang ditolak (C),
+// 10 - Jumlah kertas undi yang dikeluarkan kepada pengundi tetapi tidak dimasukkan ke dalam peti undi (D),
+// 11 - Nama Daerah Mengundi,
+// 12 - Jenis Undi
+// 13 - RAW DM - can be ignored .. orginal data from PDF
+
+// saluran is breakdown of each unique saluran
+type saluran struct {
+	ID                 string // 1 --> Daerah Mengundi ID
+	dunID              string
+	votingLocationName string // 2
+	saluranID          string // 3
+	totalVotesIssued   string // 4
+	totalVotesCast     string // 8
+	totalVotesSpoilt   string // 9
+	totalVotesNotCast  string // 10
+	daerahMengundi     string // 11
+	voteType           string // 12
+	candidateVotes     []string
+}
+
+// For 4 candidates - it is 5 + 4 + 6 = 15
+// Fixed 5 from the front
+// Dynamic candidates - x
+// Fixed 6 from the end
+
+func NewSaluranRow(prefixSlice, suffixSlice, candidatesSlice []string) saluran {
+	// DEBUG
+	//fmt.Println("PREFIX: ", prefixSlice) - FRONT
+	//fmt.Println("SUFFIX: ", suffixSlice) - BACK
+	//fmt.Println("CANDIDATES: ", candidatesSlice)
+	//fmt.Println("==============================")
+
+	// Can do validation that prefix + suffix are both 5 in len ..
+	if len(prefixSlice) != 5 || len(suffixSlice) != 6 {
+		panic("Incorrect Prefix or Suffix length")
+	}
+
+	// Process dunID which will be used later for lookup key in SPR data
+	dmID := prefixSlice[1]
+	dunID := dmID
+	if strings.Contains(dmID, "/") {
+		dunID = strings.ReplaceAll(dmID, "/", "")[0:5]
+	}
+
+	// DEBUG
+	//for i, votes := range candidatesSlice {
+	//	fmt.Println(dmID, "CANDIDATE:", dunID, "/", i, "got", votes, "in SALURAN", prefixSlice[3])
+	//}
+
+	return saluran{
+		ID:                 dmID,
+		dunID:              dunID,
+		votingLocationName: prefixSlice[2],
+		saluranID:          prefixSlice[3],
+		totalVotesIssued:   prefixSlice[4],
+		totalVotesCast:     suffixSlice[0],
+		totalVotesSpoilt:   suffixSlice[1],
+		totalVotesNotCast:  suffixSlice[2],
+		daerahMengundi:     suffixSlice[3],
+		voteType:           suffixSlice[4],
+		candidateVotes:     candidatesSlice,
+	}
+}
+
+func SanityTestPARSaluran(par string) {
+	// Open the file
+	recordFile, err := os.Open(fmt.Sprintf("testdata/%s.csv", par))
+	if err != nil {
+		fmt.Println("An error encountered ::", err)
+		panic(err)
+	}
+
+	r := csv.NewReader(recordFile)
+
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// DEBUG
+	//	fmt.Print(records)
+	var numCols, numCandidates int
+	//var salurans []saluran
+	// TODO: Have a map KEY to DMID so can do sanity check
+	for _, cols := range records {
+		// DEBUZG
+		//spew.Dump(cols[0])
+		if cols[0] == "Bil" {
+			// DEBUG
+			//fmt.Println("IN!!!")
+			numCols = len(cols)
+			numCandidates = len(cols) - 10
+			fmt.Println(fmt.Sprintf("PAR: %s NO Candidates: %d", par, numCandidates))
+
+			// Just sanity test so can move on .
+			continue
+		} else {
+			// DEBUG
+			//fmt.Println("NOT", cols[0])
+			// Verify
+			if len(cols) != numCols {
+				panic("Incorrect cols!!")
+			}
+			//x := numCols - 5
+			//y := numCols
+			// NOw can structure + append
+			//currentSaluran := NewSaluranRow(cols[0:5], cols[x:y], cols[5:x])
+			//salurans = append(salurans, currentSaluran)
+		}
+		// DEBUG
+		//		spew.Dump(cols)
+	}
+	// DEBUG
+	//	spew.Dump(salurans)
+
+}
+
+func LoadPARSaluran(par string) []saluran {
+	// Open the file
+	recordFile, err := os.Open(fmt.Sprintf("testdata/%s.csv", par))
+	if err != nil {
+		fmt.Println("An error encountered ::", err)
+		panic(err)
+	}
+
+	r := csv.NewReader(recordFile)
+
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// DEBUG
+	//	fmt.Print(records)
+	var numCols, numCandidates int
+	var salurans []saluran
+	// TODO: Have a map KEY to DMID so can do sanity check
+	for _, cols := range records {
+		// DEBUZG
+		//spew.Dump(cols[0])
+		if cols[0] == "Bil" {
+			// DEBUG
+			//fmt.Println("IN!!!")
+			numCols = len(cols)
+			numCandidates = len(cols) - 11
+			fmt.Println(fmt.Sprintf("NO Candidates: %d", numCandidates))
+		} else {
+			// DEBUG
+			//fmt.Println("NOT", cols[0])
+			// Skip metadata or empty rows!!
+			if cols[0] == "" {
+				fmt.Println("SKIP ROW since row is empty")
+				continue
+			} else if cols[0] == "JUMLAH" {
+				fmt.Println("SKIP ROW since is metadata. CHECK:", cols[3])
+				continue
+			}
+			// Verify
+			if len(cols) != numCols {
+				panic("Incorrect cols!!")
+			}
+			x := numCols - 6 // Suffix size
+			y := numCols
+			// NOw can structure + append
+			currentSaluran := NewSaluranRow(cols[0:5], cols[x:y], cols[5:x])
+			salurans = append(salurans, currentSaluran)
+		}
+		// DEBUG
+		//		spew.Dump(cols)
+	}
+	// DEBUG
+	//	spew.Dump(salurans)
+	// Do the mapping now and then output it immediately ..
+	// DEBUG
+	//parID := par[1:]
+	//fmt.Println("RESULTS: ", parID)
+	//for _, saluran := range salurans {
+	//	// Passed in the slice of votes as a Lookup method
+	//	// or output method for that row .. as csv?
+	//	// Each COALTION will fit into a certain order in the slice;
+	//	//	known a-priori per STATE
+	//	// If COALITION not there; leave as blank
+	//	keyID := fmt.Sprintf("%s/%s", parID, saluran.saluranID)
+	//	fmt.Println("LOOkUP:", keyID)
+	//}
+
+	// DEBUG
+	//s := saluran{
+	//	candidateVotes: map[string]string{
+	//		"1": "456",
+	//		"2": "123",
+	//	},
+	//}
+	//if _, ok := s.candidateVotes["100"]; ok {
+	//	fmt.Println("Found KEY 100")
+	//} else {
+	//	fmt.Println("Did NOT find KEY 100")
+	//}
+	return salurans
+}
